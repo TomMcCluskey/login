5e9cced68fe50027419e2c55156987a8f5a00012
Dan Bauer <dsbauer@gmail.com>
2015-10-14 17:37:54 -0700

    Passport demo

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var passport = require('passport');
 var app = express();
 
 // view engine setup
@@ -19,8 +21,14 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,74 @@
 var express = require('express');
 var router = express.Router();
 
+var passport = require('passport'),
+	LocalStrategy = require('passport-local');
+
+//var orch = require('orchestrate');
+//var config = require('./config');
+//var db = orch(config.dbkey);
+
+/*
+function localAuth(username,password,then) {
+	if (username.length === password.length) {
+		then({username: username, status: 'ok'});
+	} else {
+		then(null)
+	}
+}
+*/
+
+passport.use('local-signin', new LocalStrategy(
+	//{passReqToCallback : true},
+	//function(req, username, password, done) {
+	function(username, password, done) {
+		console.log("Local strategy:",username,password);
+		// db.get('passport-users',username).then(function(reply){
+		// 	var user = reply.body;
+		// 	pwd.hash(password,user.salt,function(hash){
+		// 		if (hash===user.hash) {
+					
+		// 		}
+		// 	})
+		// })
+
+		var user = (username===password) && {name: username, time: Date().toLocaleString()}
+		done(null,user);
+		// localAuth(username,password,function(user) {
+		// 	console.log('outcome: ',user);
+		// 	done(null,user);
+		// })
+	})
+)
+passport.serializeUser(function(user, done) {
+  console.log("serializing " + user.username);
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  console.log("deserializing " + obj);
+  done(null, obj);
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	if (req.user)
+ 		res.render('index', { title: 'Express', name: req.user.name, time:req.user.time });
+ 	else
+ 		res.redirect('/login');
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login');
+router.get('/login', function(req, res) {
+	res.render('login');
 });
 
-router.post('/api/login', function(req, res, next) {
-  res.send('some json!');
-});
+router.post('/api/login', passport.authenticate('local-signin', { 
+	successRedirect: '/',
+	failureRedirect: '/login'
+  })
+);
+//  res.send('some json!');
+
 
 module.exports = router;
